cad3e9adb1ef2abb91d26082ba2b64bf7b1803df
DermotCanniffe <dermot.canniffe@smartbear.com>
2019-11-20 08:19:22 +0000

    added bitbar resources in the repo root

---
+++ src/test/java/com/shoppingcart/shoppingcart/AndroidAppiumExampleTest.java
@@ -0,0 +1,40 @@
+package com.shoppingcart.shoppingcart;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+//import org.openqa.selenium.By;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+// TestNG Annotations 
+import org.testng.annotations.Test;
+
+import com.testdroid.appium.BaseAndroidTest;
+
+public class AndroidAppiumExampleTest  extends BaseAndroidTest {
+
+    @BeforeClass
+    public void setUp() throws Exception {
+        setUpTest();
+    }
+    @AfterClass
+    public void tearDown()
+    {
+        quitAppiumSession();
+    }
+
+
+    @Test
+    public void mainPageTest() throws IOException, InterruptedException {
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+        takeScreenshot("start");
+        wd.findElement(By.xpath("//android.widget.RadioButton[@text='Use Testdroid Cloud']")).click();
+        wd.findElement(By.xpath("//android.widget.EditText[@resource-id='com.bitbar.testdroid:id/editText1']")).sendKeys("John Doe");
+        takeScreenshot("after_adding_name");
+        wd.navigate().back();
+        wd.findElement(By.xpath("//android.widget.ScrollView[1]/android.widget.LinearLayout[1]/android.widget.LinearLayout[2]/android.widget.Button[1]")).click();
+        takeScreenshot("after_answer");
+    }
+
+}
